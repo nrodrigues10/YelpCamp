59e13f9682d39ae49852b3e554be92ea7f626106
Natache Rodrigues <natache_rodrigues@icloud.com>
2020-05-02 18:04:32 -0700

    create deploy v12

---
+++ v12Deploy/app.js
@@ -0,0 +1,61 @@
+var express        = require("express"),
+    app            = express(),
+    bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose"),
+    flash          = require("connect-flash"),
+    passport       = require("passport"),
+    localStrategy  = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground     = require("./models/campground"),
+    Comment        = require("./models/comment"),
+    User           = require("./models/user"),
+    seedDB         = require("./seeds")
+
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+
+mongoose.connect("mongodb://localhost/yelp_camp_v12", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB connected!'))
+  .catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
+  });
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); seed the DB
+
+//PASSWORD CONFIG
+app.use(require("express-session")({
+    secret:"I will be a great developer!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+app.listen(process.env.PORT || 3002, function(){
+    console.log("server listening")
+});
